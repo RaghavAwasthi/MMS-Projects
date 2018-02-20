b70e8478f6936e8fa72e2dec5c7c800f83c2e743
RaghavAwasthi <raghavawasthi2014@gmail.com>
2018-02-20 23:56:35 +0530

    Updated

---
+++ Class-11/src/Factorial.java
@@ -0,0 +1,13 @@
+public class Factorial {
+    public int fact(int n)
+    {
+        int result;
+        if(n==1)
+            return 1;
+        else
+        {
+            result=fact(n-1)*n;
+        }
+        return result;
+    }
+}
---
+++ Class-11/src/Justaprog.java
@@ -0,0 +1,28 @@
+/* Consider a sample Program with these requirements
+*Data Members
+* int salary-  to store salary
+* String name - to store name
+* int discount - to store discount
+*
+ */
+
+
+import java.util.Scanner;
+
+public class Justaprog {
+    int salary = 0, discount = 0;  //Data members to be declared here
+    String names;
+
+    // Scanner sc = new Scanner(System.in);  You can declare scanner here also if you want
+    Justaprog(String name) {
+        names = name;
+
+    }
+
+    public static void main(String ar[]) {
+        Scanner sc = new Scanner(System.in);
+
+        // Do whatever you want
+    }
+
+}
